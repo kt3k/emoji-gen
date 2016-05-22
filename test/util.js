3b8c3562721dbb0d9e7bd683c6570db3136d3d83
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-22 16:22:06 +0900

    test(util): add test cases of utils

---
+++ test/util.js
@@ -0,0 +1,39 @@
+const util = require('../lib/util')
+
+const test = require('tape')
+
+const path = require('path')
+
+const fixtureRoot = path.join(__dirname, 'fixture')
+
+test('util.unique makes the array contents unique', t => {
+  t.deepEqual(util.unique([1, 1, 2, 3, 2, 3, 1]), [1, 2, 3], 'The contents are unique.')
+  t.end()
+})
+
+test('util.globP returns a promise of globbed paths', t => {
+  util.globP('test/fixture/**/*.svg').then(paths => {
+    t.deepEqual(paths, [
+      'test/fixture/site/img/bar.svg',
+      'test/fixture/site/img/baz.svg',
+      'test/fixture/site/img/foo.svg'
+    ], 'It resolves with globbed paths.')
+
+    t.end()
+  })
+})
+
+test('util.getAbsPathsFromGlobs returns a promise of globbed absolute paths', t => {
+  util.getAbsPathsFromGlobs([
+    'test/fixture/**/*.svg',
+    'test/fixture/**/*.css'
+  ]).then(paths => {
+    t.deepEqual(paths, [
+      `${fixtureRoot}/site/img/bar.svg`,
+      `${fixtureRoot}/site/img/baz.svg`,
+      `${fixtureRoot}/site/img/foo.svg`,
+      `${fixtureRoot}/site/css/emoji.css`
+    ])
+    t.end()
+  })
+})
