fdd3ee4469a89d73f8c44297dd3044723ba968a4
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-22 17:46:57 +0900

    doc(cli): modify help message

--- bin/emoji-gen.js
+++ bin/emoji-gen.js
@@ -7,11 +7,13 @@ const colo = require('colo') // use colo instead of chalk
 
 const helpMessage = `
 
-  Usage: emoji-gen [paths, ...] -o [output path]
+  Usage: emoji-gen [paths, ...] -o [output path] [other options]
 
   Options:
-     --output [path]       The output css file path. This is required.
-     --class [name]        The name of emoji css class. Default is 'emoji'
+     -h, --help            Show this help message.
+     -v, --version         Show the version of the command.
+     -o, --output [path]   The output css file path. This is required.
+     --class [name]        The name of emoji css class. Default is 'emoji'.
      --sizes [size,...]    The comma separated values of sizes of generated css classes.
                            Default is '20,22,25,30'.
      --default-size [size] The value of the size of the default emoji class.
